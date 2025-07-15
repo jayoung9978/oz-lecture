4c126bb7ce95d585c7b4d2256924035695fcafaa
조자영 <jayoung9978@gmail.com>
2025-07-15 11:51:37 +0900

    day14-promise.all

--- 3.java/day14/basic/basic.js
+++ 3.java/day14/basic/basic.js
@@ -1,16 +1,25 @@
+// 비동기 개념
 // console.log("이미지 로딩 시작");
+// // console.log("이미지 로딩");
 // setTimeout(() => {
-// 	console.log("이미지 로딩");
+//   console.log("이미지 로딩");
 // }, 2000);
-// console.log("다른 작업 수행1");
-// console.log("다른 작업 수행2");
-// console.log("다른 작업 수행3");
+
+// console.log("다른 작업 수행");
 
 // 콜백함수
 function sayHello(name, callback) {
-	console.log(`안녕하세요, ${name}님!`);
-	callback();
+	console.log(`안녕, ${name}`);
+	setTimeout(() => {
+		callback();
+	}, 2000);
 }
+
+function sayHello2(name) {
+	console.log(`[2]안녕, ${name}`);
+}
+
 sayHello("학생", () => {
-	console.log("콜백함수가 호출되었습니다.");
+	console.log("콜백 실행");
 });
+sayHello2("학생");
