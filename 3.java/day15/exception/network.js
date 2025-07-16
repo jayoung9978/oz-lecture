8e6ff4c17f27bdb5f2f47139395345d61d8b4c18
조자영 <jayoung9978@gmail.com>
2025-07-16 12:28:02 +0900

    day15-json, exception

---
+++ 3.java/day15/exception/network.js
@@ -0,0 +1,14 @@
+async function getPost(postNum) {
+	try {
+		let response = await fetch(
+			`https://jsonplaceholderfdfdfdfdfdf.typicode.com/posts/${postNum}`
+		);
+		console.log(response.ok);
+		// let data = await response.json();
+		// console.log(data);
+	} catch (error) {
+		console.log(error);
+		alert("에러: " + error.message);
+	}
+}
+getPost("fdfd");
